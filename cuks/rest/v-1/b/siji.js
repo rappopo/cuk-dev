07495559cf9cc9d59939af8e477670b2f2cc9cce
Ardhi Lukianto <ardhi@lukianto.com>
2018-06-27 21:48:07 +0700

    reg update

--- cuks/rest/v-1/b/siji.js
+++ cuks/rest/v-1/b/siji.js
@@ -1,10 +1,12 @@
 'use strict'
 
 module.exports = function(cuk) {
+  const { _ } = cuk.lib
 
   return {
     method: {
       findOne: {
+        middleware: 'i18n:translation',
         handler: ctx => {
           return {
             success: true,
@@ -16,8 +18,11 @@ module.exports = function(cuk) {
         }
       },
       create: {
-        middleware: 'http:bodyParser',
+        middleware: 'http:uploadAny',
         handler: ctx => {
+          console.log(ctx.req.body)
+          console.log(ctx.req.files)
+          console.log(ctx.request.body)
           return { success: true, data: { ok: true }}
         }
       }
